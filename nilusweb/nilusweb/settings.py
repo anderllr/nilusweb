b3088b89c3c440b9287b5e463baa85821edd8bc0
michel.carvalho22 <michel.carvalho22@gmail.com>
2018-06-19 13:10:11 -0300

    funciona

--- nilusweb/nilusweb/settings.py
+++ nilusweb/nilusweb/settings.py
@@ -28,9 +28,9 @@ MEDIA_URL = '/media/'
 SECRET_KEY = '9+4q3dx3-s^uq-_eek5b)n09fnli04=op=#!gnb_fjqjqiaj$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nilus.herokuapp.com']
+# ALLOWED_HOSTS = ['nilus.herokuapp.com']
 
 
 # Application definition
